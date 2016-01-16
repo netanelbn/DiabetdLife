22139cf8eda23f8ce45e095372c7eab67c5cf2a5
netanelbn <netanelbenda7@gmail.com>
2016-01-16 21:20:20 +0200

    add file in java

---
+++ JAVA/forgets.java
@@ -0,0 +1,176 @@
+package com.parse.DiabetsApplication;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.parse.DiabetsApplication.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class forgets extends ActionBarActivity {
+
+    EditText etxtEmail;
+    Button btnSendEmail;
+    JSONParser jsonParser=new JSONParser();
+    String newPass;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgets);
+        etxtEmail=(EditText)findViewById(R.id.etxtEmailF);
+        btnSendEmail=(Button)findViewById(R.id.btnSendEmail);
+        btnSendEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseQuery<ParseObject> query = ParseQuery.getQuery("Registers");
+                query.whereEqualTo("email", etxtEmail.getText().toString());
+                query.findInBackground(new FindCallback<ParseObject>() {
+                    public void done(List<ParseObject> scoreList, ParseException e) {
+                        if (e == null) {
+                            Random rn=new Random();
+
+                            if(scoreList.size()>0)
+                            {
+                                ParseObject object=scoreList.get(0);
+                                newPass=object.getString("password");
+                                new newPassWord().execute();
+                            }
+                            else
+                            {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(forgets.this);
+                                // Specify the list in the dialog using the array
+                                builder.setTitle("Error").setMessage("This email is not register")
+                                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                                            public void onClick(DialogInterface dialog, int id) {
+
+                                            }
+                                        });
+
+                                //create and show list dialog
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+
+                        } else {
+
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_forgets, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    class newPassWord extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * Getting product details in background thread
+         */
+        protected String doInBackground(String... params) {
+
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    // Check for success tag
+                    int success;
+                    try {
+                        // Building Parameters
+                        String comment = "Hello user,\n your password is: " + newPass + "\nplease try login for change new.";
+                        List<NameValuePair> params = new ArrayList<NameValuePair>();
+                        params.add(new BasicNameValuePair("email", etxtEmail.getText().toString()));
+                        params.add(new BasicNameValuePair("subject", "diabetsLife-password recover"));
+                        params.add(new BasicNameValuePair("comment", comment));
+
+                        // getting product details by making HTTP request
+                        // Note that product details url will use GET request
+                        System.out.println("params is:" + params);
+                        JSONObject json = jsonParser.makeHttpRequest(
+                                Constrants.url_send_email, "GET", params);
+
+                        // check your log for json response
+                        Log.d("Single Product Details", json.toString());
+
+                        // json success tag
+                        success = json.getInt("success");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            return null;
+        }
+
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * *
+         */
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once got all details
+            AlertDialog.Builder builder = new AlertDialog.Builder(forgets.this);
+            // Specify the list in the dialog using the array
+            builder.setTitle("success").setMessage("Your password has send to your email. Please check!")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+
+            //create and show list dialog
+            AlertDialog dialog = builder.create();
+            dialog.show();
+
+        }
+    }
+}
---
+++ JAVA/register.java
@@ -0,0 +1,114 @@
+package com.parse.DiabetsApplication;
+
+import android.app.AlertDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.parse.DiabetsApplication.R;
+import com.parse.ParseAnalytics;
+import com.parse.ParseObject;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class register extends ActionBarActivity {
+
+    EditText etxtName,etxtEmail,etxtPassword,etxtAddress,etxtBreakTime,etxtLunch,etxtDinner;
+    Button btnRegister;
+    TextView txtLink;
+
+    /** This integer will uniquely define the dialog to be used for displaying time picker.*/
+    static final int TIME_DIALOG_ID = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+        etxtName=(EditText)findViewById(R.id.etxtName);
+        etxtEmail=(EditText)findViewById(R.id.etxtEmail);
+        etxtPassword=(EditText)findViewById(R.id.etxtPassword);
+        etxtAddress=(EditText)findViewById(R.id.etxtAddress);
+        etxtBreakTime=(EditText)findViewById(R.id.etxtBreakTime);
+        SetTime setBreakfast=new SetTime(etxtBreakTime,register.this);
+        etxtLunch=(EditText)findViewById(R.id.etxtLunch);
+        SetTime setLunch=new SetTime(etxtLunch,register.this);
+        etxtDinner=(EditText)findViewById(R.id.etxtDinner);
+        SetTime setDinner=new SetTime(etxtDinner,register.this);
+        btnRegister=(Button)findViewById(R.id.btnRRegister);
+        btnRegister.setOnClickListener(btnRegisterClick);
+        txtLink=(TextView)findViewById(R.id.link_to_login);
+        txtLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+                finish();
+            }
+        });
+    }
+    android.view.View.OnClickListener btnRegisterClick=new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            ParseObject registers = new ParseObject("Registers");
+            registers.put("username", etxtName.getText().toString());
+            registers.put("email", etxtEmail.getText().toString());
+            registers.put("password", etxtPassword.getText().toString());
+            registers.put("address", etxtAddress.getText().toString());
+            registers.put("breakfasttime", etxtBreakTime.getText().toString());
+
+            registers.put("lunchtime",  etxtLunch.getText().toString());
+
+            registers.put("dinnertime",  etxtDinner.getText().toString());
+
+            registers.saveInBackground();
+            AlertDialog.Builder builder = new AlertDialog.Builder(register.this);
+            // Specify the list in the dialog using the array
+            builder.setTitle("Done").setMessage("register is successfull")
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                        }
+                    });
+
+            //create and show list dialog
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_register, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
