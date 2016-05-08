a35318e811ed72a60236a780b07bd9780f71aad8
netanelbn <netanelbenda7@gmail.com>
2016-05-08 15:21:00 +0300

    add constrants class

---
+++ JAVA/Constrants.java
@@ -0,0 +1,6 @@
+package com.parse.DiabetsApplication;
+
+public class Constrants {
+    public static String username = "netanelproject272727@gmail.com";
+    ...public static String password = "3082862";
+}
